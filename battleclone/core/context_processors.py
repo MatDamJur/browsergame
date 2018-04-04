3c720c99e51b98c7d1a9c4825c2007cca8dc467e
Zawadeusz <zawadeusz@localhost.localdomain>
2018-04-04 20:53:18 +0200

    Fix context procesor

--- battleclone/core/context_processors.py
+++ battleclone/core/context_processors.py
@@ -2,4 +2,7 @@ from battleclone.account.models import UserProfile
 
 
 def get_user_profile(request):
-    return {'user_profile': UserProfile.objects.get(user=request.user)}
+    if request.user.is_authenticated():
+        return {'user_profile': UserProfile.objects.get(user=request.user)}
+
+    return {'user_profile': None}
